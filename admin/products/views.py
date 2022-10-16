2d7dd2a20146fb976873bacfc876e91920dc7c6d
Long.Tran <tranhuuhoanglong248@gmail.com>
2022-10-16 22:35:11 +0700

    update api

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework import status
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -15,7 +15,7 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None): # /api/products/<str:id>
         product = Product.objects.get(id=pk)
@@ -23,7 +23,11 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def update(self, request, pk=None): # /api/products/<str:id>
-        pass
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None): # /api/products/<str:id>
-        pass
+        product = Product.objects.get(id=pk)
