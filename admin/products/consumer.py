735a022291fef97284830a96cc512846d5ef8207
Long.Tran <tranhuuhoanglong248@gmail.com>
2022-10-18 00:41:35 +0700

    add pubSub to product api

---
+++ admin/products/consumer.py
@@ -0,0 +1,21 @@
+import pika
+
+params = pika.URLParameters('amqps://kkiyfxsg:kN3C7NYlYQzUbtNCAbdgU23M1vQLHyHP@armadillo.rmq.cloudamqp.com/kkiyfxsg')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='admin')
+
+def callback(channel, method, properties, body):
+    print('Receive in admin')
+    print(body)
+
+channel.basic_consume(queue='admin', on_message_callback=callback)
+
+print('Started Consuming')
+
+channel.start_consuming()
+
+channel.close()
---
+++ admin/products/producer.py
@@ -0,0 +1,10 @@
+import pika
+
+params = pika.URLParameters('amqps://kkiyfxsg:kN3C7NYlYQzUbtNCAbdgU23M1vQLHyHP@armadillo.rmq.cloudamqp.com/kkiyfxsg')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+def publish():
+    channel.basic_publish(exchange='', routing_key='admin', body='hello')
