7685e576bd3483e5e425ac79b54241a479be4f85
Long.Tran <tranhuuhoanglong248@gmail.com>
2022-10-18 22:28:52 +0700

    process mess

--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,5 @@
-import pika
+import pika, json
+from main import Product, db
 
 params = pika.URLParameters('amqps://kkiyfxsg:kN3C7NYlYQzUbtNCAbdgU23M1vQLHyHP@armadillo.rmq.cloudamqp.com/kkiyfxsg')
 
@@ -10,8 +11,23 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('Receive in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
 
+    if properties.content_type == "product_created":
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+    elif properties.content_type == "product_deleted":
+        product = Product.query.get(data['id'])
+        db.session.delete(product)
+        db.session.commit()
+        
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print('Started Consuming')
