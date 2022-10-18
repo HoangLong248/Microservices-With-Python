8122604d255401f12fdd0ca73b6ae3928028f8ef
Long.Tran <tranhuuhoanglong248@gmail.com>
2022-10-18 23:05:26 +0700

    Add host network in docker compose, Add get user in main

--- main/main.py
+++ main/main.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from sqlalchemy import UniqueConstraint
 from flask_migrate import Migrate
+from dataclasses import dataclass
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
@@ -12,11 +14,17 @@ app.app_context().push()
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
+@dataclass
 class ProductUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
@@ -24,9 +32,14 @@ class ProductUser(db.Model):
 
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
-@app.route('/')
+@app.route('/api/products')
 def index():
-    return 'hello'
+    return jsonify(Product.query.all())
 
+@app.route("/api/like", methods=["POST"])
+def like():
+    req = requests.get("http://host.docker.internal:8000/api/user")
+    return jsonify(req.json)
+    
 if __name__ == "__main__":
     app.run(debug=True)
